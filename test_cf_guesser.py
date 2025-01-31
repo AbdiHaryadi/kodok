a7f0bd8bf7f47d33689808f9a856e1f5129176c4
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2025-02-01 07:18:03 +0800

    test: using unittest library for testing

---
+++ test_cf_guesser.py
@@ -0,0 +1,60 @@
+import unittest
+
+from cf_guesser import CertaintyFactorBasedGuesser
+from entities import ObjectSpecification, QuestionAnswer
+
+class TestCertaintyFactorBasedGuesser(unittest.TestCase):
+    def test_no_update(self):
+        object_spec_list = [
+            ObjectSpecification(
+                name="Demam Berdarah Dengue (DBD)",
+                positive_questions=[
+                    "Demam?",
+                    "Demam mendadak yang tinggi?",
+                    "Suhu demam hingga 39 derajat Celcius?",
+                    "Nyeri kepala?",
+                    "Menggigil?",
+                ]
+            ),
+            ObjectSpecification(
+                name="Tipus",
+                positive_questions=[
+                    "Demam?",
+                    "Demam berlangsung lebih dari seminggu?",
+                    "Kelelahan yang berlebihan?",
+                    "Nyeri kepala?",
+                ]
+            ),
+        ]
+        guesser = CertaintyFactorBasedGuesser(object_spec_list)
+        g = guesser.guess()
+        self.assertTrue(any((g.value == x.name for x in object_spec_list)))
+        self.assertAlmostEqual(g.confidence, 0.0)
+
+    def test_single_positive_update(self):
+        object_spec_list = [
+            ObjectSpecification(
+                name="A",
+                positive_questions=["ap1"]
+            ),
+            ObjectSpecification(
+                name="B",
+                positive_questions=["bp2"]
+            )
+        ]
+
+        guesser = CertaintyFactorBasedGuesser(object_spec_list)
+        guesser.update(QuestionAnswer(
+            question="ap1",
+            answer=True
+        ))
+        g = guesser.guess()
+        guess_value = g.value
+        self.assertEqual(guess_value, "A", f"Unexpected, got \"{guess_value}\"")
+
+        confidence = g.confidence
+        expected_confidence = 1.0
+        self.assertAlmostEqual(confidence, expected_confidence, f"Expecting {expected_confidence}, got {confidence}")
+
+if __name__ == "__main__":
+    unittest.main()
