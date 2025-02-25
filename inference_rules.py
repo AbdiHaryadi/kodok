7af879bfcf9f14c54d9652efc9c72eafe55d10d8
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2025-02-25 10:25:34 +0800

    refactor(inference_rules): move general specific implementation to GeneralSpecificRule class

--- inference_rules.py
+++ inference_rules.py
@@ -12,6 +12,55 @@ class GeneralSpecificRule:
             general_questions=data["general_questions"],
             specific_questions=data["specific_questions"],
         )
+    
+    def update(self, qa_evidence_map: dict[str, bool]):
+        updated = self._negative_general_to_specific_update(qa_evidence_map)
+        updated = self._positive_specific_to_general_update(qa_evidence_map) or updated
+        return updated
+    
+    def _negative_general_to_specific_update(self, qa_evidence_map: dict[str, bool]):
+        updated = False
+
+        negate_specific_questions = False
+        for q in self.general_questions:
+            if q in qa_evidence_map and (qa_evidence_map[q] == False):
+                negate_specific_questions = True
+                break
+
+        if negate_specific_questions:
+            for q in self.specific_questions:
+                if q in qa_evidence_map:
+                    if qa_evidence_map[q] == False:
+                        continue
+
+                    raise ValueError(f"Contradiction detected: For {qa_evidence_map=}, it leads to contradiction in question {q}.")
+                
+                qa_evidence_map[q] = False
+                updated = True
+        
+        return updated
+    
+    def _positive_specific_to_general_update(self, qa_evidence_map: dict[str, bool]):
+        updated = False
+
+        satisfy_general_questions = False
+        for q in self.specific_questions:
+            if q in qa_evidence_map and (qa_evidence_map[q] == True):
+                satisfy_general_questions = True
+                break
+
+        if satisfy_general_questions:
+            for q in self.general_questions:
+                if q in qa_evidence_map:
+                    if qa_evidence_map[q] == True:
+                        continue
+
+                    raise ValueError(f"Contradiction detected: For {qa_evidence_map=}, it leads to contradiction in question {q}.")
+                
+                qa_evidence_map[q] = True
+                updated = True
+
+        return updated
 
 @dataclass
 class InferenceRules:
@@ -31,41 +80,7 @@ class InferenceRules:
         possibly_changed = True
         while possibly_changed:
             possibly_changed = False
-
-            # Negative general -> negative specific
             for rule in self.general_specific_rules:
-                negate_specific_questions = False
-                for q in rule.general_questions:
-                    if q in qa_evidence_map and (qa_evidence_map[q] == False):
-                        negate_specific_questions = True
-                        break
-
-                if negate_specific_questions:
-                    for q in rule.specific_questions:
-                        if q in qa_evidence_map:
-                            if qa_evidence_map[q] == False:
-                                continue
-
-                            raise ValueError(f"Contradiction detected: For {qa_evidence_map=}, {rule=} fired, but it leads to contradiction in question {q}.")
-                        
-                        qa_evidence_map[q] = False
-                        possibly_changed = True
-
-            # Positive specific -> Positive general
-            for rule in self.general_specific_rules:
-                satisfy_general_questions = False
-                for q in rule.specific_questions:
-                    if q in qa_evidence_map and (qa_evidence_map[q] == True):
-                        satisfy_general_questions = True
-                        break
-
-                if satisfy_general_questions:
-                    for q in rule.general_questions:
-                        if q in qa_evidence_map:
-                            if qa_evidence_map[q] == True:
-                                continue
-
-                            raise ValueError(f"Contradiction detected: For {qa_evidence_map=}, {rule=} fired, but it leads to contradiction in question {q}.")
-                        
-                        qa_evidence_map[q] = True
-                        possibly_changed = True
+                updated = rule.update(qa_evidence_map)
+                if updated:
+                    possibly_changed = True
