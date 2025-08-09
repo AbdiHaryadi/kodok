8b4ed8021409783ad534fa2f57ee6ac0d2a1c230
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2025-08-09 15:04:10 +0800

    feat: initialize Supabase

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from supabase import create_client, Client
+import time
 from experiment_3 import UnnamedState
 
 def to_proper_decimal_string(float_data):
@@ -37,20 +39,48 @@ def init_new_session():
 
     update_asked_symptom_and_answer_possibilities()
 
+@st.cache_resource
+def init_supabase():
+    url: str = st.secrets["SUPABASE_URL"]
+    key: str = st.secrets["SUPABASE_KEY"]
+    supabase: Client = create_client(url, key)
+    return supabase
 
+@st.dialog("Ubah data")
+def ask_password():
+    with st.form("pass_form", enter_to_submit=False, border=False):
+        password = st.text_input("Kata sandi", type="password")
+        if st.form_submit_button("Masuk"):
+            start_time = time.time()
+            password_correct = (password == st.secrets["ADMIN_PASS"])
+            delay = time.time() + 1 - start_time
+            if delay > 0:
+                time.sleep(delay)
 
-if "current_state" not in st.session_state:
+            if password_correct:
+                st.session_state["role"] = "admin"
+                st.rerun()
+            else:
+                st.toast("Kata sandi salah.", icon="❌")
+
+if "role" not in st.session_state:
     if "debug_mode" not in st.session_state:
         st.session_state["debug_mode"] = False
     
     if st.button("Mulai", type="primary"):
+        st.session_state["role"] = "user"
         init_new_session()
         st.rerun()
 
     new_value = st.checkbox("Mode awakutu", value=st.session_state["debug_mode"])
     st.session_state["debug_mode"] = new_value
 
-else:
+    st.divider()
+
+    if st.button("Ubah data", type="tertiary"):
+        ask_password()
+
+elif st.session_state["role"] == "user":
     current_state = st.session_state["current_state"]
 
     possibilities = st.session_state["possibilities"]
@@ -155,4 +185,33 @@ else:
     st.divider()
     if st.button("Mulai Ulang", use_container_width=True, type="tertiary"):
         init_new_session()
+        st.rerun()
+
+else:
+    supabase = init_supabase()
+    response = (
+        supabase.table("disease_symptoms")
+        .select("disease", "symptom", "variant", "frequency")
+        .execute()
+    )
+
+    sb_df = pd.DataFrame(response.data)
+
+    st.title("Ubah data")
+    symptom_tab, subsymptom_tab, other_tab = st.tabs(["Gejala Penyakit", "Anak Gejala", "???"])
+    if symptom_tab:
+        st.data_editor(sb_df, num_rows="dynamic", key="changes")
+        st.write(st.session_state["changes"])
+
+        button_cols = st.columns(2)
+        if button_cols[0].button("Simpan", type="primary", use_container_width=True):
+            st.toast("(Implementasi simpan di sini)")
+
+        if button_cols[1].button("Batal", use_container_width=True):
+            st.toast("(Implementasi batal di sini)")
+
+    st.divider()
+
+    if st.button("Keluar dari menu ubah data", type="tertiary"):
+        del st.session_state["role"]
         st.rerun()
