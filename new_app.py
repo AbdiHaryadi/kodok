ef4b9db28daf16e1c6573afee1bff8d9e323b463
AbdiHaryadi <abdiharyadi.ah@gmail.com>
2026-07-24 21:06:18 +0800

    feat: add StreamlitSymptomAsker

--- new_app.py
+++ new_app.py
@@ -35,12 +35,14 @@ class Symptom:
 
 
 class StreamlitDiscreteSymptomPropertyAsker:
-    def __init__(self, property: DiscreteSymptomProperty):
+    def __init__(self, property: DiscreteSymptomProperty, key: str | None = None):
         self.property = property
+        if key is None:
+            key = f"SymptomProperty_{self.property.get_name()}"
 
-        name = self.property.get_name()
-        if name in st.session_state:
-            self.property.set_answer(st.session_state[name])
+        self.key = key
+        if self.key in st.session_state:
+            self.property.set_answer(st.session_state[self.key])
     
     def ask(self):
         answer = self.property.get_answer()
@@ -51,10 +53,90 @@ class StreamlitDiscreteSymptomPropertyAsker:
         st.text(name)
         for answer in self.property.get_possible_answers():
             if st.button(answer):
-                st.session_state[name] = answer
-                self.property.set_answer(st.session_state[name])
+                st.session_state[self.key] = answer
+                self.property.set_answer(st.session_state[self.key])
 
-        return name in st.session_state
+        return self.key in st.session_state
+
+class StreamlitSymptomAsker:
+    def __init__(self, symptom: Symptom, key: str | None = None):
+        self.symptom = symptom
+        if key is None:
+            key = f"Symptom_{self.symptom.get_name()}"
+
+        self.key = key
+
+        self.key = key
+        if self.key in st.session_state:
+            self.symptom.set_answer(st.session_state[self.key])
+    
+    def ask(self):
+        answer = self.symptom.get_answer()
+        placeholder = st.empty()
+        if answer is None:
+            with placeholder.container():
+                self.ask_existence()
+
+        answer = self.symptom.get_answer()
+        if answer is None:
+            return
+
+        if answer is True:
+            with placeholder.container():
+                return self.ask_all_properties()
+        
+        return True
+
+    def ask_existence(self):
+        name = self.symptom.get_name()
+
+        st.text(name)
+
+        answer = None
+        if st.button("Ya", key="Symptom_Ya"):
+            answer = True
+        if st.button("Tidak", key="Symptom_Tidak"):
+            answer = False
+
+        if answer is None:
+            return False
+
+        st.session_state[self.key] = answer
+        self.symptom.set_answer(st.session_state[self.key])
+        return True
+
+    def ask_all_properties(self):
+        name = self.symptom.get_name()
+
+        st.header(name)
+        
+        properties = self.symptom.get_properties()
+
+        print("---")
+        progress = st.progress(0.0)
+
+        placeholder = st.empty()
+        for index, property in enumerate(properties):
+            with placeholder.container():
+                asked = self.ask_property(property)
+
+            if not asked:
+                return False
+
+            progress.progress((index + 1) / len(properties))
+
+            print(property.get_name(), property.get_answer())
+
+        placeholder.empty()
+        return True
+
+    def ask_property(self, property: SymptomProperty):
+        if not isinstance(property, DiscreteSymptomProperty):
+            raise NotImplementedError("Interface not supported")
+
+        asker = StreamlitDiscreteSymptomPropertyAsker(property)
+        return asker.ask()
+
 
 symptom = Symptom(
     name="Batuk",
@@ -76,48 +158,12 @@ symptom = Symptom(
     ]
 )
 
-name = symptom.get_name()
-if f"Symptom_{name}" in st.session_state:
-    symptom.set_answer(st.session_state[f"Symptom_{name}"])
-
 st.title("Kodok")
+asker = StreamlitSymptomAsker(symptom)
 
-answer = symptom.get_answer()
-if answer is None:
-    st.text(name)
-    if st.button("Ya"):
-        answer = True
-    
-    if st.button("Tidak"):
-        answer = False
+placeholder = st.empty()
+with placeholder.container():
+    asked = asker.ask()
 
-    if answer is not None:
-        st.session_state[f"Symptom_{name}"] = answer
-        st.rerun()
-
-else:
-    if answer is True:
-        st.header(name)
-
-        properties = symptom.get_properties()
-
-        print("---")
-        progress = st.progress(0.0)
-        for index, property in enumerate(properties):
-            if not isinstance(property, DiscreteSymptomProperty):
-                raise NotImplementedError("Interface not supported")
-            
-            asker = StreamlitDiscreteSymptomPropertyAsker(property)
-            placeholder = st.empty()
-            with placeholder.container():
-                asked = asker.ask()
-
-            if not asked:
-                break
-
-            progress.progress((index + 1) / len(properties))
-
-            print(property.get_name(), property.get_answer())
-            placeholder.empty()
-    
-    # else: have a nice day
+if asked:
+    placeholder.empty()
